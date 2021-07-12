5b679acb1f144649f0cef49ccc87f43af5262db7
oushihao <oushihao97@163.com>
2021-07-12 14:00:05 +0800

    style: 添加注释

--- components/WaterfallsFlow/WaterfallsFlow.vue
+++ components/WaterfallsFlow/WaterfallsFlow.vue
@@ -51,8 +51,10 @@ export default {
         };
     },
     watch: {
-        async wfList() {
-            if (this.wfList.length === 0 ||
+        // 监听列表数据变化
+        wfList() {
+            // 如果数据为空或新的列表数据少于旧的列表数据（通常为下拉刷新或切换排序或使用筛选器），初始化变量
+            if (!this.wfList.length ||
                 (this.wfList.length === this.updateNum && this.wfList.length <= this.allList.length)) {
                 this.allList = [];
                 this.leftList = [];
@@ -61,12 +63,15 @@ export default {
                 this.mark = 0;
             }
 
+            // 如果列表有值，调用waterfall方法
             if (this.wfList.length) {
                 this.allList = this.wfList;
-                await this.waterFall()
+                this.waterFall()
             }
         },
-        async mark() {
+
+        // 监听标记，当标记发生变化，则执行下一个item排序
+        mark() {
             const len = this.allList.length;
             if (this.mark < len && this.mark !== 0) {
                 this.waterFall();
@@ -74,34 +79,45 @@ export default {
         }
     },
     methods: {
+        // 瀑布流排序
         waterFall() {
             const i = this.mark;
             if (i === 0) {
+                // 初始化，从左边开始插入
                 this.leftList.push(this.allList[i]);
+                // 更新左边列表高度
                 this.getViewHeight(0);
             } else if (i === 1) {
+                // 第二个item插入，默认为右边插入
                 this.rightList.push(this.allList[i]);
+                // 更新右边列表高度
                 this.getViewHeight(1);
             } else {
+                // 根据左右列表高度判断下一个item应该插入哪边
                 const leftOrRight = this.boxHeight[0] > this.boxHeight[1] ? 1 : 0;
                 if (leftOrRight) {
                     this.rightList.push(this.allList[i])
                 } else {
                     this.leftList.push(this.allList[i])
                 }
+                // 更新插入列表高度
                 this.getViewHeight(leftOrRight);
             }
         },
+        // 获取列表高度
         getViewHeight(leftOrRight) {
             const query = uni.createSelectorQuery().in(this);
             const id = leftOrRight ? '#right' : '#left';
+            // 使用nextTick，确保页面更新结束后，再请求高度
             this.$nextTick(() => {
                 query.select(id).boundingClientRect(res => {
-                    this.boxHeight[leftOrRight] = res.height;
+                    res ? this.boxHeight[leftOrRight] = res.height : '';
                     this.mark = this.mark + 1;
                 }).exec();
             })
         },
+
+        // item点击
         itemTap(item) {
             this.$emit('itemTap', item)
         }
